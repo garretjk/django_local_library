2cf88d6bb42bc70ff86cfb65616f960d4d4699c2
Jack <jkgarrettson@gmail.com>
2025-02-17 15:23:50 -0500

    part 5

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),  # Home Page
+    path('books/', views.book_list, name='book-list'),
+    path('authors/', views.author_list, name='author-list'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book, Author
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+    return render(request, 'index.html')
+
+def book_list(request):
+    """View function for displaying all books."""
+    books = Book.objects.all()
+    return render(request, 'catalog/book_list.html', {'books': books})
+
+def author_list(request):
+    """View function for displaying all authors."""
+    authors = Author.objects.all()
+    return render(request, 'catalog/author_list.html', {'authors': authors})
