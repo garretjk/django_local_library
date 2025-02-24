15b5a5238f5f705ebfe24c4b41a4a02a478de4b7
garretjk <garretjk@dukes.jmu.edu>
2025-02-24 17:11:08 -0500

    step 7

--- catalog/views.py
+++ catalog/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
-from .models import Book, Author
+from .models import Book, Author,BookInstance
 from django.views import generic
 
 
 def index(request):
     """View function for home page of site."""
-    return render(request, 'index.html')
 
-def book_list(request):
-    """View function for displaying all books."""
-    books = Book.objects.all()
-    return render(request, 'catalog/book_list.html', {'books': books})
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
 
-def author_list(request):
-    """View function for displaying all authors."""
-    authors = Author.objects.all()
-    return render(request, 'catalog/author_list.html', {'authors': authors})
+    num_visits = request.session.get('num_visits', 0)  
+    num_visits += 1  
+    request.session['num_visits'] = num_visits 
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits, 
+    }
+
+    return render(request, 'index.html', context=context)
 
 # Book List View
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10  # Paginate results to 10 per page
+    template_name = 'book_list.html'  
 
 # Book Detail View
 class BookDetailView(generic.DetailView):
     model = Book
+    template_name = 'book_detail.html'  
 
+# Author List View
 class AuthorListView(generic.ListView):
     model = Author
+    template_name = 'author_list.html'
     paginate_by = 10  # Paginate results
 
+# Author Detail View
 class AuthorDetailView(generic.DetailView):
     model = Author
-
+    template_name = 'author_detail.html'
