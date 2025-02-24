f866ad1b8660cfedd946dce0f9542aa310fb1ca2
garretjk <garretjk@dukes.jmu.edu>
2025-02-24 15:13:28 -0500

    part 7

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Home Page
-    path('books/', views.book_list, name='book-list'),
-    path('authors/', views.author_list, name='author-list'),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+urlpatterns += [
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author
+from django.views import generic
+
 
 def index(request):
     """View function for home page of site."""
@@ -14,3 +16,20 @@ def author_list(request):
     """View function for displaying all authors."""
     authors = Author.objects.all()
     return render(request, 'catalog/author_list.html', {'authors': authors})
+
+# Book List View
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10  # Paginate results to 10 per page
+
+# Book Detail View
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10  # Paginate results
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
